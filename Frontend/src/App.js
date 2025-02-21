6c40515edd89434093c6b839f928dd0d3ef3b0c9
Eduardo Carvalho <propagno@gmail.com>
2025-02-21 19:07:18 -0300

    requirements

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,19 +7,28 @@ const App = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const code = getCodeFromUrl();
-        if (code) {
-            exchangeCodeForToken(code).then(() => {
-                window.history.replaceState({}, document.title, "/");
-                navigate('/main');
-            });
-        } else {
-            const token = localStorage.getItem('id_token');
-            if (!token) {
-                login(); // Redireciona para o Cognito Hosted UI
-            }
-        }
-    }, [navigate]);
+      console.log("App carregado");
+      const code = getCodeFromUrl();
+      console.log("Código na URL:", code);
+      if (code) {
+          console.log("Trocando código por token...");
+          exchangeCodeForToken(code).then(() => {
+              window.history.replaceState({}, document.title, "/");
+              navigate('/main');
+          });
+      } else {
+          const token = localStorage.getItem('id_token');
+          console.log("Token no localStorage:", token);
+          if (!token) {
+              console.log("Nenhum token encontrado, redirecionando para o Cognito...");
+              login(); // Deve redirecionar para o Cognito
+          } else {
+              console.log("Token encontrado, redirecionando para /main");
+              navigate('/main');
+          }
+      }
+  }, [navigate]);
+  
 
     return (
         <div>
